737ab008e5351de0b928e8f1586b8adec6c7f7c4
gaoyan <15604090129@163.com>
2017-09-02 22:54:15 +0800

    拦截器处理

--- src/main/java/com/jlt/vote/interceptor/VoteInterceptor.java
+++ src/main/java/com/jlt/vote/interceptor/VoteInterceptor.java
@@ -98,7 +98,7 @@ public class VoteInterceptor implements HandlerInterceptor {
         if(wxRedirect(chainId,uri)
                 &&BooleanUtils.isNotTrue(campaignService.checkCampaignFinish(chainId))){
             if (Objects.isNull(cookieFromOpenId)) {
-                String wxAuthUrl = wxService.buildWxAuthRedirect(chainId,request.getRequestURL().toString());
+                String wxAuthUrl = wxService.buildWxAuthRedirect(chainId,RequestUtils.getLocation(request));
                 response.sendRedirect(wxAuthUrl);
                 return false;
             }
