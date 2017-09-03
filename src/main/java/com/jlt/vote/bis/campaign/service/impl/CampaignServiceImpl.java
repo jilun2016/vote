eef7bcc87afd04ed53f4358636a52cf03580677d
gaoyan <15604090129@163.com>
2017-09-04 00:12:29 +0800

    增加普通投票 强制校验

--- src/main/java/com/jlt/vote/bis/campaign/service/impl/CampaignServiceImpl.java
+++ src/main/java/com/jlt/vote/bis/campaign/service/impl/CampaignServiceImpl.java
@@ -436,7 +436,9 @@ public class CampaignServiceImpl implements ICampaignService {
 
 	@Override
 	public int vote(Long chainId, String openId,Long userId,String ipAddress) {
-        if(Objects.isNull(openId)){
+        if(Objects.isNull(chainId)
+                ||Objects.isNull(openId)
+                ||Objects.isNull(userId)){
             logger.error("common_vote error.chainId:{},userId:{},ipAddress:{}",chainId,userId,ipAddress);
             throw new VoteRuntimeException("10000");
         }
